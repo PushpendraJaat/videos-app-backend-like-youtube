c2e68edff480f85ec101aac497a78c4a1a247e99
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2024-12-16 19:02:47 +0530

    tweet routes

---
+++ src/routes/tweet.routes.js
@@ -0,0 +1,13 @@
+import Router from "express"
+import {createTweet, deleteTweet, getUserTweets, updateTweet} from "../controllers/tweet.constroller.js"
+import {verifyJWT} from "../middlewares/auth.middleware.js"
+
+const router = Router()
+
+//secure routes
+router.route("/newTweet").post(verifyJWT, createTweet)
+router.route("/userTweets/:userId").get(verifyJWT, getUserTweets)
+router.route("/updateTweet/:tweetId").patch(verifyJWT, updateTweet)
+router.route("/deleteTweet/:tweetId").delete(verifyJWT, deleteTweet)
+
+export default router
