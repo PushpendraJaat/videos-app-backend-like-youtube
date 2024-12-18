eef23e558a583da31ba41d3e55bd9eced67c371c
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2024-12-18 19:13:14 +0530

    added routes for subscriptions

--- src/routes/subscription.routes.js
+++ src/routes/subscription.routes.js
@@ -1,11 +1,14 @@
 import { Router } from "express";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
-import { toggleSubscription } from "../controllers/subscription.controller.js";
+import { toggleSubscription, getUserChannelSubscribers, getSubscribedChannels } from "../controllers/subscription.controller.js";
 
 const router = Router()
 
 //secure routes
-router.route("/toggle-subscription/:channelId").get(verifyJWT, toggleSubscription)
+router.route("/toggle-subscription/:channelId").post(verifyJWT, toggleSubscription)
+router.route("/getUserSubscription").get(verifyJWT, getUserChannelSubscribers)
+router.route("/getUserSubscribedChannel").get(verifyJWT, getSubscribedChannels)
+
 
 
 export default router
