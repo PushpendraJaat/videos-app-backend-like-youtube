eeca91bd23192b5d78eadd5084b08531612cbb7e
Pushpendra Jaat <pushpendrajaat2001@gmail.com>
2025-03-11 23:04:16 +0530

    Enhance CORS configuration in app.js: set default origin, fix typo in credentials, and specify allowed methods and headers

--- src/app.js
+++ src/app.js
@@ -5,9 +5,11 @@ import cookieParser from "cookie-parser";
 const app = express()
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    Credential: true
-}))
+    origin: process.env.CORS_ORIGIN || '*', 
+    credentials: true, // Fixed typo
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
